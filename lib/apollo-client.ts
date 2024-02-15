a8ff411d39b447bb4fd79df1816e7fb8a68f4091
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-02-14 19:13:57 -0500

    added redirect to signin page if user is invalid

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,21 +1,41 @@
-import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
-
-const uri = process.env.NEXT_PUBLIC_GRAPHQL_API_URI || 'http://localhost:3000/graphql'
-
+import {
+	ApolloClient,
+	ApolloLink,
+	from,
+	HttpLink,
+	InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+const uri =
+	process.env.NEXT_PUBLIC_GRAPHQL_API_URI || "http://localhost:3000/graphql";
+const signinURL = process.env.SIGN_IN_URL || "http://localhost:3000/sign_in";
 function createApolloClient(context = {}) {
-  const httpLink = new HttpLink({
-    uri: uri,
-    // 'same-origin' if same origin or 'include' if cross-origin
-    credentials: 'include',
-    headers: {
-      ...context,
-    },
-  });
+	const httpLink = new HttpLink({
+		uri: uri,
+		// 'same-origin' if same origin or 'include' if cross-origin
+		credentials: "include",
+		headers: {
+			...context,
+		},
+	});
 
-  return new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
-  });
+	const errorLink = onError(({ networkError }) => {
+		if (networkError?.statusCode === 403) {
+			return window.location.replace(signinURL);
+		}
+	});
+	return new ApolloClient({
+		link: from([errorLink, httpLink]),
+		defaultOptions: {
+			query: {
+				errorPolicy: "all",
+			},
+			mutate: {
+				errorPolicy: "ignore",
+			},
+		},
+		cache: new InMemoryCache(),
+	});
 }
 
 export default createApolloClient;
