3ece09bb70a27924ead32a6119bdc66fb92585de
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-02-20 14:49:39 -0500

    create assignment form is mostly complete, needs to be added to modal and properly wired to backend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {},
+	plugins: [require("@tailwindcss/forms")],
 };
 export default config;
