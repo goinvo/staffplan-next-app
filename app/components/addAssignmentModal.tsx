b2f712ea5841fcde5895d0f835c09847cc86ef6f
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-02-21 18:43:25 -0500

    assignment modal is connected to graphql and querying data, will be slightly altered once backend has all the required fields.

--- app/components/addAssignmentModal.tsx
+++ app/components/addAssignmentModal.tsx
@@ -1,149 +1,90 @@
 "use client";
-import { useState, Fragment } from "react";
+import { useState, Fragment, useEffect } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid";
 import ProjectDatepicker from "./projectDatepicker";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
+import { gql, useQuery } from "@apollo/client";
+import withApollo from "@/lib/withApollo";
 
-const people = [
+const GET_DATA = gql`
 	{
-		id: 1,
-		name: "Wade Cooper",
-		avatar:
-			"https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 2,
-		name: "Arlene Mccoy",
-		avatar:
-			"https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 3,
-		name: "Devon Webb",
-		avatar:
-			"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
-	},
-	{
-		id: 4,
-		name: "Tom Cook",
-		avatar:
-			"https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 5,
-		name: "Tanya Fox",
-		avatar:
-			"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 6,
-		name: "Hellen Schmidt",
-		avatar:
-			"https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 7,
-		name: "Caroline Schultz",
-		avatar:
-			"https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 8,
-		name: "Mason Heaney",
-		avatar:
-			"https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 9,
-		name: "Claudie Smitham",
-		avatar:
-			"https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-	{
-		id: 10,
-		name: "Emil Schaefer",
-		avatar:
-			"https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-	},
-];
-const project = [
-	{
-		id: 1,
-		name: "Project 1",
-		startDate: "2024-01-01",
-		endDate: "2024-01-31",
-	},
-	{
-		id: 2,
-		name: "Project 2",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 3,
-		name: "Project 3",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 4,
-		name: "Project 4",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 5,
-		name: "Project 5",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 6,
-		name: "Project 6",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 7,
-		name: "Project 7",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 8,
-		name: "Project 8",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 9,
-		name: "Project 9",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-	{
-		id: 10,
-		name: "Project 10",
-		startDate: "2024-02-02",
-		endDate: "2024-02-26",
-	},
-];
+		clients {
+			id
+			projects {
+				name
+				status
+				paymentFrequency
+				users {
+					id
+					name
+					companies {
+						name
+					}
+				}
+			}
+		}
+		users {
+			id
+			name
+		}
+	}
+`;
+
 interface SelectedProject {
 	id: number;
+	paymentFrequency: string;
 	name: string;
+	status: string;
+	users: [];
 	startDate: string;
 	endDate: string;
 }
-export default function AddAssignment() {
+interface SelectedUser {
+	id: number;
+	name: string;
+}
+
+// REMOVE PLACEHOLDERS AFTER BACKEND HAS THE START AND END DATES
+const placeholderProject = {
+	id:111,
+	startDate:"2024-01-22",
+	endDate:"2024-02-24",
+	paymentFrequency: "monthly",
+	name: "PLACEHOLDER PROJECT",
+	status: "ACTIVE",
+	users: []
+}
+const placeholderDates ={
+	startDate:"2024-01-22",
+	endDate:"2024-02-24",
+}
+const AddAssignment = () => {
+	const [clientSide, setClientSide] = useState(false);
+	useEffect(() => {
+		setClientSide(true);
+	}, []);
 	const searchParams = useSearchParams();
 	const showModal = searchParams.get("modal");
-	const [selectedPerson, setSelectedPerson] = useState(people[3]);
-	const [selectedProject, setSelectedProject] = useState(project[1]);
+	const [selectedUser, setSelectedUser] = useState<SelectedUser | "">("");
+	const [selectedProject, setSelectedProject] = useState<SelectedProject | "">(
+		""
+	);
 	const handleProjectSelect = (selectedProject: SelectedProject) => {
-		setSelectedProject(selectedProject);
+		setSelectedProject({...placeholderDates,...selectedProject});
 	};
+	const { loading, error, data } = useQuery(GET_DATA, {
+		context: {
+			headers: {
+				cookie: clientSide ? document.cookie : null,
+			},
+		},
+		skip: !clientSide,
+		errorPolicy: "all",
+	});
+	if (loading) return <p> LOADING</p>;
+	if (error) return <p>ERROR</p>;
 	return (
 		<>
 			{showModal && (
@@ -164,8 +105,8 @@ export default function AddAssignment() {
 											<form className="max-w-lg mx-auto">
 												<div className="flex mb-4">
 													<Listbox
-														value={selectedPerson}
-														onChange={setSelectedPerson}
+														value={selectedUser}
+														onChange={setSelectedUser}
 													>
 														{({ open }) => (
 															<>
@@ -177,7 +118,9 @@ export default function AddAssignment() {
 																		<Listbox.Button className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
 																			<span className="flex items-center">
 																				<span className="ml-3 block truncate">
-																					{selectedPerson.name}
+																					{selectedUser
+																						? selectedUser.name
+																						: "Select"}
 																				</span>
 																			</span>
 																			<span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -197,48 +140,50 @@ export default function AddAssignment() {
 																		leaveTo="opacity-0"
 																	>
 																		<Listbox.Options className="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-																			{people.map((person) => (
-																				<Listbox.Option
-																					key={person.id}
-																					className={({ active }) =>
-																						active
-																							? "bg-indigo-600 text-white"
-																							: "text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
-																					}
-																					value={person}
-																				>
-																					{({ selected, active }) => (
-																						<>
-																							<div className="flex items-center">
-																								<span
-																									className={
-																										selected
-																											? "font-semibold"
-																											: "font-normal ml-3 block truncate"
-																									}
-																								>
-																									{person.name}
-																								</span>
-																							</div>
+																			{data?.users?.map(
+																				(user: SelectedUser) => (
+																					<Listbox.Option
+																						key={user.id}
+																						className={({ active }) =>
+																							active
+																								? "bg-indigo-600 text-white"
+																								: "text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
+																						}
+																						value={user}
+																					>
+																						{({ selected, active }) => (
+																							<>
+																								<div className="flex items-center">
+																									<span
+																										className={
+																											selected
+																												? "font-semibold"
+																												: "font-normal ml-3 block truncate"
+																										}
+																									>
+																										{user.name}
+																									</span>
+																								</div>
 
-																							{selected ? (
-																								<span
-																									className={
-																										active
-																											? "text-white"
-																											: "text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4"
-																									}
-																								>
-																									<CheckIcon
-																										className="h-5 w-5"
-																										aria-hidden="true"
-																									/>
-																								</span>
-																							) : null}
-																						</>
-																					)}
-																				</Listbox.Option>
-																			))}
+																								{selected ? (
+																									<span
+																										className={
+																											active
+																												? "text-white"
+																												: "text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4"
+																										}
+																									>
+																										<CheckIcon
+																											className="h-5 w-5"
+																											aria-hidden="true"
+																										/>
+																									</span>
+																								) : null}
+																							</>
+																						)}
+																					</Listbox.Option>
+																				)
+																			)}
 																		</Listbox.Options>
 																	</Transition>
 																</div>
@@ -259,7 +204,9 @@ export default function AddAssignment() {
 																		<Listbox.Button className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
 																			<span className="flex items-center">
 																				<span className="ml-3 block truncate">
-																					{selectedProject.id}
+																					{selectedProject
+																						? selectedProject.name
+																						: "Select"}
 																				</span>
 																			</span>
 																			<span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -279,48 +226,52 @@ export default function AddAssignment() {
 																		leaveTo="opacity-0"
 																	>
 																		<Listbox.Options className="absolute z-10 mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-																			{project.map((project) => (
-																				<Listbox.Option
-																					key={project.id}
-																					className={({ active }) =>
-																						active
-																							? "bg-indigo-600 text-white"
-																							: "text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
-																					}
-																					value={project}
-																				>
-																					{({ selected, active }) => (
-																						<>
-																							<div className="flex items-center">
-																								<span
-																									className={
-																										selected
-																											? "font-semibold"
-																											: "font-normal ml-3 block truncate"
-																									}
-																								>
-																									{project.id}
-																								</span>
-																							</div>
+																			{data?.clients?.map((client: { projects: SelectedProject[]; }) => {
+																				return client.projects.map(
+																					(project: SelectedProject) => (
+																						<Listbox.Option
+																							key={`${project.id} + ${project.name}`}
+																							className={({ active }) =>
+																								active
+																									? "bg-indigo-600 text-white"
+																									: "text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9"
+																							}
+																							value={project}
+																						>
+																							{({ selected, active }) => (
+																								<>
+																									<div className="flex items-center">
+																										<span
+																											className={
+																												selected
+																													? "font-semibold"
+																													: "font-normal ml-3 block truncate"
+																											}
+																										>
+																											{project.name}
+																										</span>
+																									</div>
 
-																							{selected ? (
-																								<span
-																									className={
-																										active
-																											? "text-white"
-																											: "text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4"
-																									}
-																								>
-																									<CheckIcon
-																										className="h-5 w-5"
-																										aria-hidden="true"
-																									/>
-																								</span>
-																							) : null}
-																						</>
-																					)}
-																				</Listbox.Option>
-																			))}
+																									{selected ? (
+																										<span
+																											className={
+																												active
+																													? "text-white"
+																													: "text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4"
+																											}
+																										>
+																											<CheckIcon
+																												className="h-5 w-5"
+																												aria-hidden="true"
+																											/>
+																										</span>
+																									) : null}
+																								</>
+																							)}
+																						</Listbox.Option>
+																					)
+																				);
+																			})}
 																		</Listbox.Options>
 																	</Transition>
 																</div>
@@ -355,7 +306,8 @@ export default function AddAssignment() {
 															</div>
 														</div>
 													</div>
-													<ProjectDatepicker projectDates={selectedProject} />
+													{/* RESET TO SELECTED PROJECT ONCE BACKEND HAS START AND END DATE */}
+													<ProjectDatepicker projectDates={selectedProject ? selectedProject : placeholderProject} />
 												</div>
 
 												<div className="flex mb-4 justify-between">
@@ -401,4 +353,5 @@ export default function AddAssignment() {
 			)}
 		</>
 	);
-}
+};
+export default withApollo(AddAssignment);
