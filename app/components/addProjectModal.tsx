730aea38d6eda9fc737e322ec792faa31ec5ad83
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-03-04 17:44:09 -0500

    fixed import/export mistake

--- app/components/addProjectModal.tsx
+++ app/components/addProjectModal.tsx
@@ -412,7 +412,7 @@ const AddProject = () => {
 															className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 															onClick={handleSubmit}
 														>
-															Assign
+															Save
 														</Link>
 													</div>
 												</div>
