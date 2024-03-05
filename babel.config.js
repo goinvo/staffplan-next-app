eaf475ed691a0d6a3383e83ad98b7cb6fae5730e
MyStarrySpace <25332257+MyStarrySpace@users.noreply.github.com>
2024-03-05 12:57:11 -0500

    Added tests for navbar contents

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', // Transpile modern JS to compatible JS
+    '@babel/preset-react', // Transform JSX
+    '@babel/preset-typescript', // For using TypeScript
+  ],
+};
