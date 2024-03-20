42b82691c565190106cb8d36910718ca8f4867a4
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-03-20 15:45:15 -0400

    loading spinner in center of page when loading

--- app/components/loadingSpinner.tsx
+++ app/components/loadingSpinner.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {CSSProperties} from "react";
 import SyncLoader from "react-spinners/SyncLoader";
 
 export const LoadingSpinner = () => {
+    const override: CSSProperties = {
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      };
 	return (
 		<SyncLoader
 			color={"#72DDC3"}
 			loading
+            cssOverride={override}
 			size={15}
 			aria-label="Loading Spinner"
 			data-testid="loader"
