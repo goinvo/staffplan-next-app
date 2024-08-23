a68cf00776987bbb42b9fc7504b16913b38d2143
Oleksandr Zhukov <sashazh316@gmail.com>
2024-08-23 19:03:14 +0300

    added edit user form

--- app/components/v2/scrollingCalendar/components/iconButton/iconButton.tsx
+++ app/components/v2/scrollingCalendar/components/iconButton/iconButton.tsx
@@ -7,13 +7,15 @@ interface IconButtonProps {
     ariaLabel?: string
     text?: string
     iconSize?: string
+    type?: 'submit' | 'button' | 'reset'
 }
-const IconButton: React.FC<IconButtonProps> = ({ Icon, onClick, iconSize, className, ariaLabel, text }) => {
+const IconButton: React.FC<IconButtonProps> = ({ Icon, onClick, iconSize, className, ariaLabel, text, type = 'button' }) => {
     return (
         <button
             onClick={onClick}
             className={className}
             aria-label={ariaLabel}
+            type={type}
         >
             <div className='flex self-start py-0.5'>{Icon && <Icon className={iconSize} />}</div>
             {text && <span className='px-1'>
