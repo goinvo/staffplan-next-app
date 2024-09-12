28ec016e35256ba10e46ef125edd26c5c669c724
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-09-12 17:05:29 -0400

    fixed so environment is correct

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,10 +3,7 @@ import React from "react";
 import Link from "next/link";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { FaSquarePlus } from "react-icons/fa6";
-import {
-	ChatBubbleBottomCenterTextIcon,
-} from "@heroicons/react/24/outline";
-
+import { ChatBubbleBottomCenterTextIcon } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
 import { useUserDataContext } from "../userDataContext";
 import { LoadingSpinner } from "./loadingSpinner";
@@ -14,13 +11,11 @@ import { LoadingSpinner } from "./loadingSpinner";
 import IconButton from "./iconButton";
 import NewPersonAndProjectModal from "./newPersonAndProjectModal";
 import { useModal } from "../modalContext";
-
 const Navbar: React.FC = () => {
 	const { viewer } = useUserDataContext();
 	const fullPathName = usePathname();
 	const pathname = usePathname().split("/")[1];
-	const { openModal, closeModal } = useModal()
-
+	const { openModal, closeModal } = useModal();
 	if (!viewer) return <LoadingSpinner />;
 
 	const myPlanUrl = () => {
@@ -30,18 +25,28 @@ const Navbar: React.FC = () => {
 	const myStaffPlanCheck = fullPathName.match(myStaffPlanURL);
 
 	const links = [
-		{ href: `/people/${myPlanUrl()}`, label: "My StaffPlan", isActive: myStaffPlanCheck },
-		{ href: "/people", label: "People", isActive: pathname === "people" && !myStaffPlanCheck },
+		{
+			href: `/people/${myPlanUrl()}`,
+			label: "My StaffPlan",
+			isActive: myStaffPlanCheck,
+		},
+		{
+			href: "/people",
+			label: "People",
+			isActive: pathname === "people" && !myStaffPlanCheck,
+		},
 		{ href: "/projects", label: "Projects", isActive: pathname === "projects" },
 	];
-
+	const homepageUrl = process.env.NEXT_PUBLIC_NODE_ENV ? "http://localhost:3000" : "https://staffplan.com";
 	const additionalLinks = [
-		{href:"https://github.com/goinvo/staffplan-next-app", label: 'Open Source' },
-		{ href:"mailto:staffplan@goinvo.com",label: 'Feedback' },
-		{href:"https://staffplan.com/settings", label: 'Settings' },
-		{href:"https://staffplan.com/sign_out", label: 'Sign Out' }
-	]
-
+		{
+			href: "https://github.com/goinvo/staffplan-next-app",
+			label: "Open Source",
+		},
+		{ href: "mailto:staffplan@goinvo.com", label: "Feedback" },
+		{ href: `${homepageUrl}/settings`, label: "Settings" },
+		{ href: `${homepageUrl}/sign_out`, label: "Sign Out" },
+	];
 	return (
 		<nav className="navbar bg-gray-100 px-5 h-14 flex justify-between items-center">
 			<div className="flex items-center space-x-4 h-full">
@@ -49,12 +54,23 @@ const Navbar: React.FC = () => {
 					<Link
 						key={link.href}
 						href={link.href}
-						className={`inline-flex items-center text-base px-4 py-2 rounded-md ${link.isActive ? 'bg-contrastBlue font-semibold' : "hover:bg-contrastBlue"}`}
+						className={`inline-flex items-center text-base px-4 py-2 rounded-md ${
+							link.isActive
+								? "bg-contrastBlue font-semibold"
+								: "hover:bg-contrastBlue"
+						}`}
 					>
 						{link.label}
 					</Link>
 				))}
-				<IconButton Icon={FaSquarePlus} onClick={() => openModal(<NewPersonAndProjectModal closeModal={closeModal} />)} iconSize="h-8 w-8 rounded-md text-contrastGrey" className="px-8" />
+				<IconButton
+					Icon={FaSquarePlus}
+					onClick={() =>
+						openModal(<NewPersonAndProjectModal closeModal={closeModal} />)
+					}
+					iconSize="h-8 w-8 rounded-md text-contrastGrey"
+					className="px-8"
+				/>
 			</div>
 
 			<div className="flex ml-auto items-center bg-contrastBlue rounded-2xl overflow-hidden min-w-[225px] h-8">
@@ -70,14 +86,13 @@ const Navbar: React.FC = () => {
 			<div className="flex items-center space-x-4 py-4 ml-2">
 				{additionalLinks.map((link) => (
 					<Link
-					key={link.href}
-					href={link.href}
-					className={`inline-flex items-center text-base px-4 py-2 rounded-md hover:bg-contrastBlue`}
-				>
-					{link.label}
-				</Link>
-				)
-				)}
+						key={link.href}
+						href={link.href}
+						className={`inline-flex items-center text-base px-4 py-2 rounded-md hover:bg-contrastBlue`}
+					>
+						{link.label}
+					</Link>
+				))}
 				<div className="h-4 w-4">
 					<Link href="?airTableFormModal=true">
 						<ChatBubbleBottomCenterTextIcon />
