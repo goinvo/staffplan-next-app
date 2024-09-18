4d91c9517b645976d1b3a6a420a7f46751146bbf
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-09-18 12:32:24 -0400

    archive button now works on projectsAndAssignments

--- app/components/userSummary.tsx
+++ app/components/userSummary.tsx
@@ -6,6 +6,8 @@ import { ArchiveBoxIcon } from "@heroicons/react/24/outline";
 import { UserSummaryProps } from "../typeInterfaces";
 import { useUserDataContext } from "../userDataContext";
 import IconButton from "./iconButton";
+import { UPSERT_ASSIGNMENT } from "../gqlQueries";
+import { useMutation } from "@apollo/client";
 
 const UserSummary: React.FC<UserSummaryProps> = ({ assignment }) => {
 	const { viewsFilter } = useUserDataContext();
@@ -13,6 +15,7 @@ const UserSummary: React.FC<UserSummaryProps> = ({ assignment }) => {
 		(acc, curr) => acc + (curr.actualHours ?? 0),
 		0
 	);
+	const {refetchUserList} = useUserDataContext();
 	const pastPlan = () => {
 		const now = DateTime.now();
 		const startOfAssignment = DateTime.fromISO(assignment.startsOn ?? "");
@@ -29,6 +32,25 @@ const UserSummary: React.FC<UserSummaryProps> = ({ assignment }) => {
 			return assignment.estimatedWeeklyHours * Math.ceil(timeBetween.weeks);
 		}
 	};
+	const [upsertAssignment] = useMutation(UPSERT_ASSIGNMENT, {
+		errorPolicy: "all",
+		onCompleted({ upsertAssignment }) {
+			refetchUserList();
+		},
+	});
+	const handleArchiveItemClick = () => {
+		if (assignment.status !== 'archived') {
+			const variables = {
+				id: assignment.id,
+				projectId: assignment.project.id,
+				userId: assignment.assignedUser.id,
+				status: 'archived'
+			};
+			upsertAssignment({
+				variables
+			})
+		}
+	}
 	return (
 		<td className="font-normal py-2 pl-4 w-1/6 flex items-center justify-between">
 			{viewsFilter.showSummaries ? (
@@ -62,7 +84,9 @@ const UserSummary: React.FC<UserSummaryProps> = ({ assignment }) => {
 			) : null}
 			<div className="flex items-start justify-center">
 				<IconButton className='text-black flex items-start justify-center text-transparentGrey'
-					onClick={() => console.log('On archive box btn click')}
+					onClick={() => {
+						console.log('On archive box btn click')
+						handleArchiveItemClick()}}
 					Icon={ArchiveBoxIcon}
 					iconSize={'h6 w-6'} />
 			</div>
