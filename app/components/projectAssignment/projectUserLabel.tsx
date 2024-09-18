935aa1836f5320d78140955776dd798f6054f1b5
Oleksandr Zhukov <sashazh316@gmail.com>
2024-09-18 15:34:22 +0300

    added toggle functionality for assignment status

--- app/components/projectAssignment/projectUserLabel.tsx
+++ app/components/projectAssignment/projectUserLabel.tsx
@@ -1,15 +1,37 @@
 import Image from "next/image";
 import React from "react";
+
+import { UPSERT_ASSIGNMENT } from "../../gqlQueries";
 import { UserLabelProps } from "@/app/typeInterfaces";
 import { AddPersonInline } from "../addPersonInline";
+import { useUserDataContext } from "@/app/userDataContext";
+import { useMutation } from "@apollo/client";
 
 export const ProjectUserLabel = ({
 	project,
 	assignment,
 	clickHandler,
 }: UserLabelProps) => {
+	const { refetchProjectList } = useUserDataContext();
 	const isUserTBD = assignment.assignedUser === null;
+	const [upsertAssignment] = useMutation(UPSERT_ASSIGNMENT, {
+		errorPolicy: "all",
+		onCompleted() {
+			refetchProjectList();
+		},
+	});
 
+	const onChangeStatusButtonClick = async () => {
+		const variables = {
+			id: assignment.id,
+			projectId: project?.id,
+			userId: assignment.assignedUser.id,
+			status: assignment.status === "active" ? "proposed" : "active",
+		};
+		await upsertAssignment({
+			variables
+		})
+	}
 	return (
 		<td className='px-0 pr-0 pt-2 pb-2 font-normal flex align-center w-1/3'>
 			<div
@@ -34,21 +56,13 @@ export const ProjectUserLabel = ({
 							(<button className="px-2" onClick={() => clickHandler(assignment)}>
 								{assignment.assignedUser.name}
 							</button>)}
-						<div>
-							{assignment.status !== 'active' && !isUserTBD &&
-								(
-									<span className="px-2 text-red-500 font-normal">
-										Unconfirmed
-									</span>
-								)}
-						</div>
 					</div>
 				</div>
 			</div>
 			<div className='text-contrastBlue flex flex-col space-y-3 ml-auto px-2 items-end max-w-[60px]'>
-				<div className='pt-1 underline'>
+				<button className='pt-1 underline' onClick={onChangeStatusButtonClick}>
 					{assignment.status === 'proposed' ? 'Proposed' : 'Signed'}
-				</div>
+				</button>
 				<div className='pt-2'>
 					Actual
 				</div>
