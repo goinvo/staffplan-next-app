2562b0243ee316fcac7183fbd974942edf12c9e4
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-10-29 16:18:17 -0400

    altered the ellipsis dropdown to be more flexible, added ellipsis dropdown to assignment in mystaffplan

--- app/components/ellipsisDropdownMenu.tsx
+++ app/components/ellipsisDropdownMenu.tsx
@@ -1,21 +1,25 @@
-import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import { Fragment, ReactNode } from "react";
+import { Menu,MenuItem, MenuButton, MenuItems, Transition } from "@headlessui/react";
 import React from "react";
-import Link from "next/link";
 
 interface DropdownMenuProps {
-    options: { label: string, href: string, show?: boolean }[];
+    options: {  component:()=>ReactNode,show?: boolean }[];
+    location: 'navbar' | 'userAssignment';
 }
 
-const EllipsisDropdownMenu: React.FC<DropdownMenuProps> = ({ options }) => {
+const EllipsisDropdownMenu: React.FC<DropdownMenuProps> = ({ options, location }) => {
+    const ellipsisENUM = {
+        navbar: "text-white",
+        userAssignment: "text-gray-900",
+    }
     const dropdownSelectedItemClass = (isActive: boolean) => {
         return isActive
             ? "px-4 py-2 block hover:text-tiffany hover:cursor-pointer text-sm"
             : "text-gray-900 block px-4 py-2 text-sm";
     };
     return (
-        <Menu as="div" className="relative inline-block text-left z-20">
-            <Menu.Button className="flex items-center text-white hover:text-gray-300 p-2 rounded-full ellipsismenu focus:outline-none" />
+        <Menu as="div" className="relative inline-block text-left z-15">
+            <MenuButton className={`flex items-center ${ellipsisENUM[location]} hover:text-gray-300 p-2 rounded-full ellipsismenu focus:outline-none`} />
             <Transition
                 as={Fragment}
                 enter="transition ease-out duration-100"
@@ -25,24 +29,21 @@ const EllipsisDropdownMenu: React.FC<DropdownMenuProps> = ({ options }) => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 mt-2 w-48 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-20">
+                <MenuItems className="absolute right-0 mt-2 w-48 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-20">
                     <div className="py-1">
-                        {options.map((option, index) =>
-                            option.href && option.show ? (
-                                <Menu.Item key={index}>
+                    {options.map((option, index) =>
+                            option.show ? (
+                                <MenuItem key={index}>
                                     {({ active }) => (
-                                        <Link
-                                            href={option.href}
-                                            className={dropdownSelectedItemClass(active)}
-                                        >
-                                            {option.label}
-                                        </Link>
+                                        <div className={dropdownSelectedItemClass(active)}>
+                                            {option.component()}
+                                        </div>
                                     )}
-                                </Menu.Item>
+                                </MenuItem>
                             ) : null
                         )}
                     </div>
-                </Menu.Items>
+                </MenuItems>
             </Transition>
         </Menu>
     );
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -43,18 +43,31 @@ const Navbar: React.FC = () => {
 		},
 		{ href: "/projects", label: "Projects", isActive: pathname === "projects" },
 	];
-	const ellipsisLinks = [
+	const ellipsisDropdownMenuOption = (href: string, label: string) => {
+		return <Link href={href}>{label}</Link>;
+	};
+	const ellipsisDropdownMenuOptions = [
 		{
-			href: "https://github.com/goinvo/staffplan-next-app",
-			label: "Open Source",
+			component:()=> ellipsisDropdownMenuOption(
+				"https://github.com/goinvo/staffplan-next-app",
+				"Open Source"
+			),
 			show: true,
 		},
 		{
-			href: `${homepageUrl}/settings/profile`,
-			label: "Settings",
+			component:()=> ellipsisDropdownMenuOption(
+				`${homepageUrl}/settings/profile`,
+				"Settings"
+			),
 			show: true,
 		},
-		{ href: `${homepageUrl}/sign_out`, label: "Sign Out", show: true },
+		{
+			component:()=> ellipsisDropdownMenuOption(
+				`${homepageUrl}/sign_out`,
+				"Sign Out"
+			),
+			show: true,
+		},
 	];
 	return (
 		<nav className="navbar bg-gray-100 px-5 h-14 flex justify-between items-center">
@@ -63,10 +76,11 @@ const Navbar: React.FC = () => {
 					<Link
 						key={link.href}
 						href={link.href}
-						className={`inline-flex items-center text-base sm:px-4 px-1 sm:py-2 py-1 rounded-md whitespace-nowrap ${link.isActive
-							? "bg-contrastBlue font-semibold"
-							: "hover:bg-contrastBlue"
-							}`}
+						className={`inline-flex items-center text-base sm:px-4 px-1 sm:py-2 py-1 rounded-md whitespace-nowrap ${
+							link.isActive
+								? "bg-contrastBlue font-semibold"
+								: "hover:bg-contrastBlue"
+						}`}
 					>
 						{link.label}
 					</Link>
@@ -95,12 +109,14 @@ const Navbar: React.FC = () => {
 				<div>
 					<button
 						className="sm:inline-flex hidden items-center text-base px-4 py-2 rounded-md hover:bg-contrastBlue"
-						onClick={() => openModal(<AirTableFormModal closeModal={closeModal} />)}
+						onClick={() =>
+							openModal(<AirTableFormModal closeModal={closeModal} />)
+						}
 					>
 						Feedback
 					</button>
 				</div>
-				<EllipsisDropdownMenu options={ellipsisLinks} />
+				<EllipsisDropdownMenu options={ellipsisDropdownMenuOptions} location={"navbar"} />
 				{/* Temporary commented */}
 				{/* <div className="h-4 w-4">
 					<ChatBubbleBottomCenterTextIcon />
