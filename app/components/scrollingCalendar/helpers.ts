a8d071aa79a759247c50c42717d9c0c1ba0243c5
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-10-30 14:11:00 -0400

    removed console log

--- app/components/scrollingCalendar/helpers.ts
+++ app/components/scrollingCalendar/helpers.ts
@@ -979,7 +979,6 @@ export const updateOrInsertWorkWeekInProject = (
             const canBeDeleted = !updatedWorkWeeks.some(
               (week) => (week.actualHours ?? 0) > 0
             );
-            console.log("canBeDeleted",canBeDeleted,'in helper')
             const updatedFields = {
               workWeeks: updatedWorkWeeks,
               canBeDeleted:canBeDeleted
