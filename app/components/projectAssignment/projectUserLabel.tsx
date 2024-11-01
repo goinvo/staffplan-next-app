866c5fba74afaf7d194595ec960184c55a4ec3e2
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2024-11-01 11:31:23 -0400

    created left hand gutter to hold menu

--- app/components/projectAssignment/projectUserLabel.tsx
+++ app/components/projectAssignment/projectUserLabel.tsx
@@ -16,11 +16,16 @@ export const ProjectUserLabel = ({
 	assignment,
 	clickHandler,
 }: ProjectLabelProps) => {
-	const { refetchProjectList } = useProjectsDataContext()
+	const { refetchProjectList } = useProjectsDataContext();
 	const { openModal, closeModal } = useModal();
-	const { viewer } = useGeneralDataContext()
+	const { viewer } = useGeneralDataContext();
 	const isUserTBD = assignment.assignedUser === null;
-	const { projectList, setProjectList, singleProjectPage, setSingleProjectPage } = useProjectsDataContext();
+	const {
+		projectList,
+		setProjectList,
+		singleProjectPage,
+		setSingleProjectPage,
+	} = useProjectsDataContext();
 	const [upsertAssignment] = useMutation(UPSERT_ASSIGNMENT, {
 		errorPolicy: "all",
 		onCompleted() {
@@ -29,10 +34,17 @@ export const ProjectUserLabel = ({
 	});
 	const canAssignmentBeDeleted = !assignment.workWeeks.some(
 		(week) => (week.actualHours ?? 0) > 0
-	  );
-	  const showActionsButton = viewer?.id === assignment.assignedUser?.id || assignment.assignedUser === null;
-	  const showArchiveButton = viewer?.id === assignment.assignedUser?.id && !canAssignmentBeDeleted
-	  const showDeleteButton = !assignment.assignedUser || viewer?.id === assignment.assignedUser?.id && canAssignmentBeDeleted && assignment.canBeDeleted;
+	);
+	const showActionsButton =
+		viewer?.id === assignment.assignedUser?.id ||
+		assignment.assignedUser === null;
+	const showArchiveButton =
+		viewer?.id === assignment.assignedUser?.id && !canAssignmentBeDeleted;
+	const showDeleteButton =
+		!assignment.assignedUser ||
+		(viewer?.id === assignment.assignedUser?.id &&
+			canAssignmentBeDeleted &&
+			assignment.canBeDeleted);
 	const [deleteAssignment] = useMutation(DELETE_ASSIGNMENT, {
 		errorPolicy: "all",
 		onCompleted({ deleteAssignment }) {
@@ -40,10 +52,11 @@ export const ProjectUserLabel = ({
 			const updatedAssignments = singleProjectPage?.assignments?.filter(
 				(assignment) => assignment.id !== deletedAssignmentId
 			);
-			singleProjectPage && setSingleProjectPage({
-				...singleProjectPage,
-				assignments: updatedAssignments,
-			});
+			singleProjectPage &&
+				setSingleProjectPage({
+					...singleProjectPage,
+					assignments: updatedAssignments,
+				});
 			const projectId = singleProjectPage?.id;
 			if (projectList && projectId) {
 				const updatedProjectList = projectList.map((project) => {
@@ -69,8 +82,8 @@ export const ProjectUserLabel = ({
 			};
 			deleteAssignment({ variables });
 			return;
+		}
 	};
-	}
 	const onChangeStatusButtonClick = async () => {
 		const variables = {
 			id: assignment.id,
@@ -79,20 +92,20 @@ export const ProjectUserLabel = ({
 			status: assignment.status === "active" ? "proposed" : "active",
 		};
 		await upsertAssignment({
-			variables
-		})
-	}
+			variables,
+		});
+	};
 	const handleArchiveAssignmentClick = async () => {
 		const variables = {
 			id: assignment.id,
 			projectId: project?.id,
 			userId: assignment.assignedUser.id,
-			status: 'archived',
+			status: "archived",
 		};
 		await upsertAssignment({
-			variables
-		})
-	}
+			variables,
+		});
+	};
 	const assignmentDropMenuOptions = [
 		{
 			component: (
@@ -114,45 +127,62 @@ export const ProjectUserLabel = ({
 			show: true,
 		},
 		{
-			component: <button onClick={handleArchiveAssignmentClick} className="text-gray-900 block px-4 py-2 text-sm">Archive</button>,
+			component: (
+				<button
+					onClick={handleArchiveAssignmentClick}
+					className="text-gray-900 block px-4 py-2 text-sm"
+				>
+					Archive
+				</button>
+			),
 			show: showArchiveButton && showActionsButton,
 		},
 		{
-			component: <button onClick={handleDeleteAssignmentClick} className="text-gray-900 block px-4 py-2 text-sm">Delete</button>,
+			component: (
+				<button
+					onClick={handleDeleteAssignmentClick}
+					className="text-gray-900 block px-4 py-2 text-sm"
+				>
+					Delete
+				</button>
+			),
 			show: showDeleteButton && showActionsButton,
 		},
 	];
 	return (
-		<td className='px-0 pr-0 pt-2 pb-2 font-normal flex align-center sm:w-1/3 w-1/2'>
-			<div
-				className='flex flex-row justify-between sm:items-start items-center'
-			>
-				{isUserTBD &&
-					<AddPersonInline project={project} assignment={assignment} />
-				}
-					<div className="hover:cursor-pointer ml-auto sm:flex hidden">
-						{showActionsButton && <EllipsisDropdownMenu
-				options={assignmentDropMenuOptions}
-				textColor={"text-gray-900"}
-				menuPositioning="relative -top-2.5"
-				/>}
+		<td className="px-0 pr-0 pt-2 pb-2 font-normal flex align-center sm:w-1/3 w-1/2">
+			<div className="flex flex-row justify-between sm:items-start items-center">
+				<div className="w-12 ml-auto sm:flex hidden">
+					{showActionsButton && (
+						<EllipsisDropdownMenu
+							options={assignmentDropMenuOptions}
+							textColor={"text-gray-900"}
+							menuPositioning="relative -top-2.5"
+						/>
+					)}
 				</div>
-				<div className="w-48 font-bold flex text-contrastBlue w-full">
+				{isUserTBD && (
+					<AddPersonInline project={project} assignment={assignment} />
+				)}
 
-					{!isUserTBD && (<div className="py-2 relative overflow-hidden w-[38px] h-[28px]  aspect-w-38 aspect-h-28">
-						<Image
-							src={assignment.assignedUser.avatarUrl}
-							className="rounded-md object-cover"
-							alt="user avatar"
-							fill
-							sizes="(max-width: 640px) 28px, (max-width: 768px) 38px, 38px"
+				<div className="w-48 font-bold flex text-contrastBlue w-full">
+					{!isUserTBD && (
+						<div className="py-2 relative overflow-hidden w-[38px] h-[28px]  aspect-w-38 aspect-h-28">
+							<Image
+								src={assignment.assignedUser.avatarUrl}
+								className="rounded-md object-cover"
+								alt="user avatar"
+								fill
+								sizes="(max-width: 640px) 28px, (max-width: 768px) 38px, 38px"
 							/>
-					</div>)}
+						</div>
+					)}
 					<div className="flex flex-col items-center justify-center">
-						{!isUserTBD &&
-							(<button className="px-2" onClick={() => clickHandler(assignment)}>
+						{!isUserTBD && (
+							<button className="px-2" onClick={() => clickHandler(assignment)}>
 								{assignment.assignedUser.name}
-							</button>)}
+							</button>
+						)}
 					</div>
 				</div>
 				{/* {showActionsButton && <EllipsisDropdownMenu
@@ -160,14 +190,12 @@ export const ProjectUserLabel = ({
 				textColor={"text-gray-900"}
 			/>} */}
 			</div>
-			<div className='text-contrastBlue sm:flex hidden flex-col space-y-3 ml-auto px-2 items-end max-w-[60px]'>
-				<button className='pt-1 underline' onClick={onChangeStatusButtonClick}>
-					{assignment.status === 'proposed' ? 'Proposed' : 'Signed'}
+			<div className="text-contrastBlue sm:flex hidden flex-col space-y-3 ml-auto px-2 items-end max-w-[60px]">
+				<button className="pt-1 underline" onClick={onChangeStatusButtonClick}>
+					{assignment.status === "proposed" ? "Proposed" : "Signed"}
 				</button>
-				<div className='pt-2'>
-					Actual
-				</div>
+				<div className="pt-2">Actual</div>
 			</div>
-		</td >
+		</td>
 	);
 };
