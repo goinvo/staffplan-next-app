2968fbbce0563c10100f854ab83ddac012d5b93b
Bohdan Remeniuk <31549198+zhukovdigital@users.noreply.github.com>
2024-12-05 08:48:37 +0200

    Make user photo as link

--- app/components/allUsers/allUserLabel.tsx
+++ app/components/allUsers/allUserLabel.tsx
@@ -7,7 +7,10 @@ export const AllUserLabel = ({ user, clickHandler }: AllUserLabelProps) => {
 	return (
 		<td className='pr-2 pl-0 pt-1 pb-2 font-normal flex align-center sm:w-1/3 w-1/2'>
 			<div className="sm:w-48 pl-1 font-bold flex items-center justify-start text-contrastBlue">
-				<div className="px-2 py-2 relative overflow-hidden w-[38px] h-[28px] aspect-w-38 aspect-h-28">
+				<div
+					className="px-2 py-2 relative overflow-hidden w-[38px] h-[28px] aspect-w-38 aspect-h-28 cursor-pointer"
+					onClick={() => clickHandler(user)}
+				>
 					<Image
 						src={`${decodeURIComponent(user.avatarUrl)}`}
 						alt="client avatar"
