6d85776431942c0db3e271b062df51f85474468b
Bohdan Remeniuk <31549198+zhukovdigital@users.noreply.github.com>
2024-12-26 09:37:09 +0200

    Redirect owner admin after + button click

--- app/people/page.tsx
+++ app/people/page.tsx
@@ -2,14 +2,22 @@
 import React, { useState } from "react";
 import withApollo from "@/lib/withApollo";
 
+import { useRouter } from "next/navigation";
 import { AssignmentType, UserType } from "../typeInterfaces";
 import { LoadingSpinner } from "../components/loadingSpinner";
 import { ScrollingCalendar } from "../components/scrollingCalendar/scrollingCalendar";
 import { AllUserRow } from "../components/allUsers/allUserRow";
 import { useUserDataContext } from "../contexts/userDataContext";
 import { SORT_ORDER } from "../components/scrollingCalendar/constants";
+import { useGeneralDataContext } from "../contexts/generalContext";
 
 const PeopleView: React.FC = () => {
+  const router = useRouter();
+
+  const homepageUrl = process.env.NEXT_PUBLIC_NODE_ENV
+    ? "http://localhost:3000"
+    : "https://app.staffplan.com";
+
   const [initialSorting, setInitialSorting] = useState<{title: string; sort: SORT_ORDER}>(() => {
     if (typeof window !== "undefined" && localStorage) {
       const savedInitialSorting = localStorage.getItem("peoplePageSorting");
@@ -19,9 +27,18 @@ const PeopleView: React.FC = () => {
     }
   });
 
-	const { userList } = useUserDataContext();
+  const { userList } = useUserDataContext();
+  const { viewer } = useGeneralDataContext();
 
-	const columnHeaderTitles = [{ title: 'People', showIcon: true }]
+	const columnHeaderTitles = [
+    {
+      title: "People",
+      showIcon: viewer?.role === "admin" || viewer?.role === "owner",
+      ...(viewer?.role === "admin" || viewer?.role === "owner"
+        ? { onIconClick: () => router.push(`${homepageUrl}/settings/users`) }
+        : {}),
+    },
+  ];
 
 	const getAllAssignments = (users: UserType[]): AssignmentType[] => {
 		return users?.flatMap(user =>
