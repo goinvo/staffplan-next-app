f91b5928f4f2f74bf31f582305c0a10925ef16f7
Corbin Campbell <114195647+HankC138@users.noreply.github.com>
2025-01-02 15:54:57 -0500

    remove undo string

--- app/components/constants/undoModifyStrings.ts
+++ app/components/constants/undoModifyStrings.ts
@@ -2,4 +2,4 @@ export const UNDO_ARCHIVED_OR_DELETED_PROJECT = "Keep this project";
 export const UNDO_ARCHIVED_PROJECT_TITLE = "Project archived for everyone";
 export const UNDO_ARCHIVED_PROJECT_SUBTITLE = "Archiving removes the project from everyones view, including its hours.";
 export const UNDO_ARCHIVED_OR_DELETED_PERSON = "Undo, I still need this person";
-export const UNDO_DELETED_PERSON_TITLE = "Deleted {name} and their proposed/plan hours from this project Undo";
+export const UNDO_DELETED_PERSON_TITLE = "Deleted {name} and their proposed/plan hours from this project";
