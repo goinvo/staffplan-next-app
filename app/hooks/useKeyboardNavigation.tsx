199c9a0ad61b88078214bb370f12e685129ab440
Bohdan Remeniuk <31549198+zhukovdigital@users.noreply.github.com>
2025-01-08 13:56:46 +0200

    add keyboard shortcut for new project

--- app/hooks/useKeyboardNavigation.tsx
+++ app/hooks/useKeyboardNavigation.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useGeneralDataContext } from "../contexts/generalContext";
 import { MonthsDataType } from "../typeInterfaces";
+import { useModal } from "../contexts/modalContext";
+import NewPersonAndProjectModal from "../components/newPersonAndProjectModal";
 
 type KeyboardNavigation = {
   getNextWeeksPerView: (months: MonthsDataType[]) => string;
@@ -18,6 +20,7 @@ export const useKeyboardNavigation = ({
 }: KeyboardNavigation) => {
   const router = useRouter();
 
+  const { openModal, closeModal } = useModal();
   const { viewer, isInputInFocus } = useGeneralDataContext();
 
   useEffect(() => {
@@ -26,16 +29,20 @@ export const useKeyboardNavigation = ({
     const handleKeyDown = (event: KeyboardEvent) => {
       switch (event.key.toLowerCase()) {
         case "m":
-          if(isInputInFocus) return;
+          if (isInputInFocus) return;
           router.push(`/people/${encodeURIComponent(viewer.id)}`);
           break;
         case "p":
-          if(isInputInFocus) return;
+          if (isInputInFocus) return;
           router.push(`/projects`);
           break;
         case "e":
-          if(isInputInFocus) return;
+          if (isInputInFocus) return;
           router.push(`/people`);
+          break;
+        case "n":
+          if (isInputInFocus) return;
+          openModal(<NewPersonAndProjectModal closeModal={closeModal} />);
           break;
         case "arrowright":
           if (!isInputInFocus) setDateRange(getNextWeeksPerView(months));
